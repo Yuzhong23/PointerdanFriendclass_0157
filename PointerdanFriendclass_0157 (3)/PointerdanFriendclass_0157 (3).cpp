f174f7ed7ed63b0d010cea388ee84fd5e0db93be
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-07 11:13:02 +0700

    Membuat void orang dan siswa

--- PointerdanFriendclass_0157 (3)/PointerdanFriendclass_0157 (3).cpp
+++ PointerdanFriendclass_0157 (3)/PointerdanFriendclass_0157 (3).cpp
@@ -28,6 +28,13 @@ void siswa::displayAll(orang& a)
 	cout << id << endl << a.nama;
 }
 
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
 
 int main()
 {
