33eadd3b794698a9afd992338d57f5b0367431d2
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-07 11:32:13 +0700

    Membuat isi dari class manusia dan void manusia

--- PointerdanFriendclass_0157 (2)/PointerdanFriendclass_0157 (5)/PointerdanFriendclass_0157 (5).cpp
+++ PointerdanFriendclass_0157 (2)/PointerdanFriendclass_0157 (5)/PointerdanFriendclass_0157 (5).cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 class pelajar;
-class manusia;
+class manusia
+{
+public:
+	void showNilaipelajar(pelajar &x);
+};
 
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::showNilaipelajar(pelajar &x);
+};
+
+void manusia::showNilaipelajar(pelajar &x)
+{
+	cout << x.nilai;
+}
 
 int main() {
 
