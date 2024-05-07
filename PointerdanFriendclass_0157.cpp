0c9ea8c4b7d2917797871f406d1ba8f84a004a95
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-07 10:47:44 +0700

    Membuat referensi refMhs untuk merujuk pada objek mhs

--- PointerdanFriendclass_0157.cpp
+++ PointerdanFriendclass_0157.cpp
@@ -11,6 +11,8 @@ public:
 };
 
 int main() {
-    mahasiswa mhs{ 1 }; //objek mhs
-    mhs.showNim();   
+    mahasiswa mhs{ 1 }; // objek mhs
+    mhs.showNim(); 
+
+    mahasiswa& refMhs = mhs; 
 }
